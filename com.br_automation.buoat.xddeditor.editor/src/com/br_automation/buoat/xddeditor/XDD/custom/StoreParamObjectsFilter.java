7e8fc5937e253c15deff14998d516e7383e09736
Joris Lueckenga <joris.lueckenga@br-automation.com>
2013-03-26 10:43:18 +0100

    [FEATURE] Add PropertySection for subobjects of object 0x1010.
    - AdvancedStoreParamPropertySection defines the PropertySection for the
    TabbedProperty view.
    - StoreParamObjectsFilter to filter for store-parameter objects, having
    a parent index of 0x1010 and subindex > 0
    - StoreParamComposite defines 3 radio buttons to calculate and set the
    default value. Used in StoreParam property-section.
    - TObjectComposite is a new class to get and set mandatory data
    (index/subindex, object type and name) and is implemented in
    StoreParam property-section.
    
    Change-Id: I8a767f97b7fba47edb72bc78ba74a1acf89652b4

---
+++ com.br_automation.buoat.xddeditor.editor/src/com/br_automation/buoat/xddeditor/XDD/custom/StoreParamObjectsFilter.java
@@ -0,0 +1,40 @@
+package com.br_automation.buoat.xddeditor.XDD.custom;
+
+import java.math.BigInteger;
+
+import org.eclipse.jface.viewers.IFilter;
+
+import com.br_automation.buoat.xddeditor.XDD.SubObjectType;
+import com.br_automation.buoat.xddeditor.XDD.TObject;
+
+/**
+ * @author Joris Lückenga
+ * @since 25.3.2013
+ * @brief Filters for SubobjectTypes of Index 1010 and Subindex &gt; 1
+ */
+public class StoreParamObjectsFilter implements IFilter {
+
+    @Override
+    public boolean select(Object toTest) {
+
+        if (toTest instanceof SubObjectType) {
+
+            SubObjectType toTest2 = (SubObjectType) toTest;
+            TObject parentObject = (TObject) toTest2.eContainer();
+            int result;
+
+            if (parentObject.getIndex() != null) {
+                result = new BigInteger(1, parentObject.getIndex()).intValue();
+
+                if (result == 0x1010) {
+                    int test = new BigInteger(toTest2.getSubIndex()).intValue();
+                    if (test != 0)
+                        return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+}
---
+++ com.br_automation.buoat.xddeditor.editor/src/com/br_automation/buoat/xddeditor/XDD/custom/TObjectComposite.java
@@ -0,0 +1,245 @@
+package com.br_automation.buoat.xddeditor.XDD.custom;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.xml.type.internal.DataValue.HexBin;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+import com.br_automation.buoat.xddeditor.XDD.SubObjectType;
+import com.br_automation.buoat.xddeditor.XDD.TObject;
+import com.br_automation.buoat.xddeditor.XDD.custom.XDDUtilities.RegexVerifyListener;
+import com.br_automation.buoat.xddeditor.XDD.provider.SubObjectTypeItemProvider;
+import com.br_automation.buoat.xddeditor.XDD.provider.TObjectItemProvider;
+
+/**
+ * @author Joris Lückenga
+ * @since 25.3.2013
+ * @brief Composite for TObjects or SubObjectTypes
+ */
+public class TObjectComposite extends Composite {
+    private final Combo cmbObjectType;
+    private final FocusAdapter indexFocusListener = new FocusAdapter() {
+        @Override
+        public void focusLost(FocusEvent event) {
+
+            if (TObjectComposite.this.stautsTObject) {
+                TObjectComposite.this.tobjectitemProvider.setPropertyValue(
+                    TObjectComposite.this.tobject, "index",
+                    HexBin.decode(TObjectComposite.this.txtIndex.getText()));
+            } else {
+                TObjectComposite.this.subObjectItemProvicer.setPropertyValue(
+                    TObjectComposite.this.subobject, "subIndex",
+                    HexBin.decode(TObjectComposite.this.txtIndex.getText()));
+            }
+        }
+    };
+    private final RegexVerifyListener indexVerifyListener = new RegexVerifyListener(
+        RegexVerifyListener.PATTERN_HEX, Arrays.asList(
+            Character.valueOf((char) 0x7f), Character.valueOf((char) 0x8)), true);
+
+    private final Label lblindex;
+    private final FocusAdapter nameListener = new FocusAdapter() {
+        @Override
+        public void focusLost(FocusEvent event) {
+            if (TObjectComposite.this.stautsTObject)
+                TObjectComposite.this.tobjectitemProvider.setPropertyValue(
+                    TObjectComposite.this.tobject, "name", TObjectComposite.this.txtName.getText());
+            else
+                TObjectComposite.this.subObjectItemProvicer.setPropertyValue(
+                    TObjectComposite.this.subobject, "name",
+                    TObjectComposite.this.txtName.getText());
+        }
+    };
+
+    private final ModifyListener objectTypeListener = new ModifyListener() {
+
+        @Override
+        public void modifyText(ModifyEvent event) {
+            String selectionStr = TObjectComposite.this.cmbObjectType.getText();
+            short selection;
+            if (selectionStr.contentEquals("7 - VAR"))
+                selection = 7;
+            else if (selectionStr.contentEquals("8 - ARRAY"))
+                selection = 8;
+            else if (selectionStr.contentEquals("9 - RECORD"))
+                selection = 9;
+            else
+                selection = 0;
+
+            if (TObjectComposite.this.stautsTObject && selection != 0)
+                TObjectComposite.this.tobjectitemProvider.setPropertyValue(
+                    TObjectComposite.this.tobject, "objectType", selection);
+            else if (selection != 0)
+                TObjectComposite.this.subObjectItemProvicer.setPropertyValue(
+                    TObjectComposite.this.subobject, "objectType", selection);
+
+        }
+    };
+
+    private boolean stautsTObject;
+
+    private SubObjectType subobject;
+
+    private final SubObjectTypeItemProvider subObjectItemProvicer;
+    private TObject tobject;
+    private final TObjectItemProvider tobjectitemProvider;
+
+    private final Text txtIndex;
+    private final Text txtName;
+
+    /**
+     * Create the composite.
+     * 
+     * @param parent
+     *            see Composite#Composite(Composite, int)
+     * @param style
+     *            see Composite#Composite(Composite, int)
+     * @param factory
+     *            to set properties for TObject/Subobjects
+     */
+    public TObjectComposite(Composite parent,
+        int style,
+        AdapterFactory factory) {
+        super(parent, style);
+        this.tobjectitemProvider = new TObjectItemProvider(factory);
+        this.subObjectItemProvicer = new SubObjectTypeItemProvider(factory);
+        this.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
+        Group grpMandatory = new Group(this, SWT.NONE);
+        grpMandatory.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        grpMandatory.setText("Mandatory Data");
+        grpMandatory.setBounds(10, 0, 271, 112);
+
+        Label lblName = new Label(grpMandatory, SWT.NONE);
+        lblName.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        this.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblName.setBounds(10, 22, 55, 15);
+        lblName.setText("Name:");
+
+        this.lblindex = new Label(grpMandatory, SWT.NONE);
+        this.lblindex.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        this.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        this.lblindex.setText("Index:");
+        this.lblindex.setBounds(10, 80, 55, 15);
+
+        this.txtName = new Text(grpMandatory, SWT.BORDER);
+        this.txtName.setBounds(83, 19, 183, 21);
+
+        this.txtIndex = new Text(grpMandatory, SWT.BORDER | SWT.RIGHT);
+        this.txtIndex.setBounds(83, 77, 55, 21);
+        this.txtIndex.setTextLimit(4);
+
+        this.cmbObjectType = new Combo(grpMandatory, SWT.NONE);
+        this.cmbObjectType.setItems(new String[] { "7 - VAR", "8 - ARRAY", "9 - RECORD" });
+        this.cmbObjectType.setBounds(83, 45, 183, 23);
+
+        Label lblObjectType = new Label(grpMandatory, SWT.NONE);
+        lblObjectType.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblObjectType.setBounds(10, 48, 67, 15);
+        lblObjectType.setText("Object Type:");
+
+        Label lblH = new Label(grpMandatory, SWT.NONE);
+        lblH.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblH.setBounds(144, 80, 7, 15);
+        lblH.setText("h");
+
+    }
+
+    /**
+     * @brief sets the TObject or SubObjectType of the composite also sets given
+     *        parameters.
+     * @param object
+     *            EObject of Type TObject or SubObject
+     */
+    public void setObject(final EObject object) {
+        this.removeListeners();
+        if (object instanceof TObject) {
+            this.txtIndex.setTextLimit(4);
+            this.lblindex.setText("Index:");
+            this.tobject = (TObject) object;
+
+            if (this.tobject.getIndex() != null) {
+                String result = Integer.toHexString(new BigInteger(1, this.tobject.getIndex())
+                    .intValue());
+                this.txtIndex.setText(result);
+            }
+            if (this.tobject.getName() != null)
+                this.txtName.setText(this.tobject.getName());
+            int i = 0;
+            for (String objectType : this.cmbObjectType.getItems()) {
+                if (objectType.contains(Short.toString(this.tobject.getObjectType()))) {
+                    this.cmbObjectType.select(i);
+                    break;
+                } else
+                    this.cmbObjectType.setText("Type not found!");
+                i++;
+            }
+
+        } else {
+            this.txtIndex.setTextLimit(2);
+            this.subobject = (SubObjectType) object;
+            this.lblindex.setText("Subindex:");
+
+            if (this.subobject.getSubIndex() != null) {
+                String result = String.format(
+                    "%02x", (new BigInteger(1, this.subobject.getSubIndex())).intValue())
+                    .toUpperCase();
+                this.txtIndex.setText(result);
+            }
+            if (this.subobject.getName() != null)
+                this.txtName.setText(this.subobject.getName());
+
+            int i = 0;
+            for (String objectType : this.cmbObjectType.getItems()) {
+                if (objectType.contains(Short.toString(this.subobject.getObjectType()))) {
+                    this.cmbObjectType.select(i);
+                    break;
+                } else {
+                    this.cmbObjectType.setText("Type not found!");
+                }
+                i++;
+            }
+        }
+        this.addListeners();
+    }
+
+    @Override
+    protected void checkSubclass() {
+        // Disable the check that prevents subclassing of SWT components
+    }
+
+    /**
+     * @brief adds the Focus/Verfy and Modifylisteners when needed
+     */
+    private void addListeners() {
+
+        this.txtName.addFocusListener(this.nameListener);
+        this.txtIndex.addFocusListener(this.indexFocusListener);
+        this.txtIndex.addVerifyListener(this.indexVerifyListener);
+        this.cmbObjectType.addModifyListener(this.objectTypeListener);
+
+    }
+
+    /**
+     * @brief removes the Focus/Verfy and Modifylisteners when needed
+     */
+    private void removeListeners() {
+        this.txtName.removeFocusListener(this.nameListener);
+        this.txtIndex.removeFocusListener(this.indexFocusListener);
+        this.txtIndex.removeVerifyListener(this.indexVerifyListener);
+        this.cmbObjectType.removeModifyListener(this.objectTypeListener);
+    }
+}
