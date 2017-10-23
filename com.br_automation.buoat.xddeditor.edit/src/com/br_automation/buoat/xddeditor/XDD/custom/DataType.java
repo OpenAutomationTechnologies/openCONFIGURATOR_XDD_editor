49c41a8ed69f26392720b0fe20a42eb0e57ccc4c
Sree Hari Vignesh <sreeharivignesh.b@kalycito.com>
2017-10-23 16:56:23 +0530

    [FIX] Update compliance level of Java to 1.8
     - Fix format issues with source files
     - Update the execution environment to 1.8
    
    Change-Id: I5a1783fa77935c722feff25e22db43c78ca4a700
    Signed-off-by: Sree Hari Vignesh <sreeharivignesh.b@kalycito.com>

--- com.br_automation.buoat.xddeditor.edit/src/com/br_automation/buoat/xddeditor/XDD/custom/DataType.java
+++ com.br_automation.buoat.xddeditor.edit/src/com/br_automation/buoat/xddeditor/XDD/custom/DataType.java
@@ -1,7 +1,31 @@
 /**
  * @since 19.3.2013
  * @author Joris Lückenga, B&R Industrial Automation GmbH
- */
+ *
+ * @copyright (c) 2017, B&R Industrial Automation GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the copyright holders nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 
 package com.br_automation.buoat.xddeditor.XDD.custom;
 
@@ -16,14 +40,26 @@ package com.br_automation.buoat.xddeditor.XDD.custom;
  */
 public enum DataType {
 
-    BOOLEAN(8, "Boolean", 0x1), INTEGER16(16, "Integer16", 0x3), INTEGER24(24, "Integer24", 0x10), INTEGER32(32,
-            "Integer32", 0x4), INTEGER40(40, "Integer40", 0x12), INTEGER48(48, "Integer48", 0x13), INTEGER56(56,
-                    "Integer56", 0x14), INTEGER64(64, "Integer64", 0x15), INTEGER8(8, "Integer8", 0x2), REAL32(32,
-                            "Real32", 0x8), REAL64(64, "Real64", 0x11), UNKNOWN(0, "Unknown", 0xFFFF), UNSIGNED16(16,
-                                    "Unsigned16", 0x6), UNSIGNED24(24, "Unsigned24", 0x16), UNSIGNED32(32, "Unsigned32",
-                                            0x7), UNSIGNED40(40, "Unsigned40", 0x18), UNSIGNED48(48, "Unsigned48",
-                                                    0x19), UNSIGNED56(56, "Unsigned56", 0x1A), UNSIGNED64(64,
-                                                            "Unsigned64", 0x1B), UNSIGNED8(8, "Unsigned8", 0x5);
+    BOOLEAN(8, "Boolean", 0x1),
+    INTEGER16(16, "Integer16", 0x3),
+    INTEGER24(24, "Integer24", 0x10),
+    INTEGER32(32, "Integer32", 0x4),
+    INTEGER40(40, "Integer40", 0x12),
+    INTEGER48(48, "Integer48", 0x13),
+    INTEGER56(56, "Integer56", 0x14),
+    INTEGER64(64, "Integer64", 0x15),
+    INTEGER8(8, "Integer8", 0x2),
+    REAL32(32, "Real32", 0x8),
+    REAL64(64, "Real64", 0x11),
+    UNKNOWN(0, "Unknown", 0xFFFF),
+    UNSIGNED16(16, "Unsigned16", 0x6),
+    UNSIGNED24(24, "Unsigned24", 0x16),
+    UNSIGNED32(32, "Unsigned32", 0x7),
+    UNSIGNED40(40, "Unsigned40", 0x18),
+    UNSIGNED48(48, "Unsigned48", 0x19),
+    UNSIGNED56(56, "Unsigned56", 0x1A),
+    UNSIGNED64(64, "Unsigned64", 0x1B),
+    UNSIGNED8(8, "Unsigned8", 0x5);
 
     private int bitSize;
     private String typeName;
--- com.br_automation.buoat.xddeditor.editor/src/com/br_automation/buoat/xddeditor/XDD/custom/CiADeviceProfile.java
+++ com.br_automation.buoat.xddeditor.editor/src/com/br_automation/buoat/xddeditor/XDD/custom/CiADeviceProfile.java
@@ -36,37 +36,27 @@ package com.br_automation.buoat.xddeditor.XDD.custom;
  */
 public enum CiADeviceProfile {
 
-    CIA401(401, "CiA 401", "Generic I/O Modules"), CIA402(402, "CiA 402", "Drives and Motion Control"), CIA404(404,
-            "CiA 404", "Measuring devices and Closed Loop Controllers"), CIA405(405, "CiA 405",
-                    "IEC 61131-3 Programmable Devices"), CIA406(406, "CiA 406", "Rotating and Linear Encoders"), CIA408(
-                            408, "CiA 408", "Hydraulic Drives and Proportional Valves"), CIA410(410, "CiA 410",
-                                    "Inclinometers"), CIA412(412, "CiA 412", "Medical Devices"), CIA413(413, "CiA 413",
-                                            "Truck Gateways"), CIA414(414, "CiA 414",
-                                                    "Yarn Feeding Units (Weaving Machines)"), CIA415(415, "CiA 415",
-                                                            "Road Construction Machinery"), CIA416(416, "CiA 416",
-                                                                    "Building Door Control"), CIA417(417, "CiA 417",
-                                                                            "Lift Control Systems"), CIA418(418,
-                                                                                    "CiA 418",
-                                                                                    "Battery Modules"), CIA419(419,
-                                                                                            "CiA 419",
-                                                                                            "Battery Chargers"), CIA420(
-                                                                                                    420, "CiA 420",
-                                                                                                    "Extruder Downstream Devices"), CIA422(
-                                                                                                            422,
-                                                                                                            "CiA 422",
-                                                                                                            "Municipal Vehicles CleANopen"), CIA423(
-                                                                                                                    423,
-                                                                                                                    "CiA 423",
-                                                                                                                    "Railway Diesel Control Systems"), CIA424(
-                                                                                                                            424,
-                                                                                                                            "CiA 424",
-                                                                                                                            "Rail Vehicle Door Control Systems"), CIA425(
-                                                                                                                                    425,
-                                                                                                                                    "CiA 425",
-                                                                                                                                    "Medical Diagnostic Add-on Modules"), CIA445(
-                                                                                                                                            445,
-                                                                                                                                            "CiA 445",
-                                                                                                                                            "RFID Devices");
+    CIA401(401, "CiA 401", "Generic I/O Modules"),
+    CIA402(402, "CiA 402", "Drives and Motion Control"),
+    CIA404(404, "CiA 404", "Measuring devices and Closed Loop Controllers"),
+    CIA405(405, "CiA 405", "IEC 61131-3 Programmable Devices"),
+    CIA406(406, "CiA 406", "Rotating and Linear Encoders"),
+    CIA408(408, "CiA 408", "Hydraulic Drives and Proportional Valves"),
+    CIA410(410, "CiA 410", "Inclinometers"),
+    CIA412(412, "CiA 412", "Medical Devices"),
+    CIA413(413, "CiA 413", "Truck Gateways"),
+    CIA414(414, "CiA 414", "Yarn Feeding Units (Weaving Machines)"),
+    CIA415(415, "CiA 415", "Road Construction Machinery"),
+    CIA416(416, "CiA 416", "Building Door Control"),
+    CIA417(417, "CiA 417", "Lift Control Systems"),
+    CIA418(418, "CiA 418", "Battery Modules"),
+    CIA419(419, "CiA 419", "Battery Chargers"),
+    CIA420(420, "CiA 420", "Extruder Downstream Devices"),
+    CIA422(422, "CiA 422", "Municipal Vehicles CleANopen"),
+    CIA423(423, "CiA 423", "Railway Diesel Control Systems"),
+    CIA424(424, "CiA 424", "Rail Vehicle Door Control Systems"),
+    CIA425(425, "CiA 425", "Medical Diagnostic Add-on Modules"),
+    CIA445(445, "CiA 445", "RFID Devices");
 
     private String profileDescription;
     private String profileName;
